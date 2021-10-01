4c0af522e708b913ae863d0bb628a2028afc4d8f
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2021-10-01 16:53:23 +0200

    initial commit with basic pwa testing and apollo config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+	// You should use an absolute URL here
+	uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+})
+
+createApp({
+	setup () {
+		provide(DefaultApolloClient, apolloClient)
+	},
+
+	render: () => h(App),
+}).use(router).mount('#app')
