1bafd2020845a002e403f84c60f8b7ccd2061308
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2021-12-08 15:33:12 -0800

    some style

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,13 +1,15 @@
 <template>
-	<div class="container is-fluid">
+	<div class="container is-fluid 	single-page-wrapper">
 		<section class="section">
 			<div class="hero-body">
-				<p class="title mb-3">Welcome to Dual!</p>
-				<p>
-					<RouterLink to="/login">Login</RouterLink> |
-					<RouterLink to="/signup">Signup</RouterLink>
-				</p>
+				<span class="logo has-text-success">
+					D.C
+				</span>
 			</div>
+		</section>
+		<section>
+			<RouterLink to="/login" class="button mr-4">Login</RouterLink>
+			<RouterLink to="/signup" class="button">Signup</RouterLink>
 		</section>
 	</div>
 </template>
@@ -17,3 +19,9 @@ export default {
 	name: 'Welcome',
 }
 </script>
+
+<style type="text/scss">
+.logo {
+	font-size: 6rem;
+}
+</style>
