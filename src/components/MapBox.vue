dc1624dafb516d3431765049bb5e53d0efe03228
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2023-09-15 13:25:18 -0700

    retrieving user's lat lon

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -16,9 +16,25 @@ export default {
 		async createMap () {
 			mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN
 
+			const { coords: { latitude = 0, longitude = 0 } } = await new Promise(
+				(resolve) => {
+					navigator.geolocation.getCurrentPosition(
+						resolve,
+						() => resolve({ coords: {} }),
+						{
+							enableHighAccuracy: true
+						}
+					)
+				}
+			)
+
+			console.log('yoooooo', latitude, longitude)
+
 			this.map = new mapboxgl.Map({
 				container: 'map',
 				style: 'mapbox://styles/mapbox/streets-v11',
+				center: [longitude, latitude],
+				zoom: 12
 			})
 		}
 	}
