3a20c7e5b3f7a608fdd6d5929fd33e0eeb7fb695
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2023-09-15 17:08:12 -0700

    client side marker added

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -6,6 +6,15 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl'
 
+const markers = [
+	{
+		lat: 47.6120157,
+		lon: -122.305772,
+		title: 'A thing!',
+		description: 'A description of the thing!',
+	}
+]
+
 export default {
 	name: 'MapBox',
 	props: {},
@@ -16,11 +25,11 @@ export default {
 		async createMap () {
 			mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN
 
-			const { coords: { latitude = 0, longitude = 0 } } = await new Promise(
+			const { coords: { latitude = 0, longitude = 0 } = {} } = await new Promise(
 				(resolve) => {
 					navigator.geolocation.getCurrentPosition(
 						resolve,
-						() => resolve({ coords: {} }),
+						() => resolve({}),
 						{
 							enableHighAccuracy: true
 						}
@@ -28,23 +37,44 @@ export default {
 				}
 			)
 
-			console.log('yoooooo', latitude, longitude)
-
 			this.map = new mapboxgl.Map({
 				container: 'map',
 				style: 'mapbox://styles/mapbox/streets-v11',
 				center: [longitude, latitude],
-				zoom: 12
+				zoom: 14
+			})
+
+			markers.forEach(({ lat, lon, title, description }) => {
+				const markerEl = document.createElement('div')
+				markerEl.classList.add('marker')
+
+				new mapboxgl.Marker(markerEl)
+					.setLngLat([lon, lat])
+					.setPopup(
+						new mapboxgl.Popup({ offset: 25 })
+							.setHTML(
+								`<h3>${title}</h3><p>${description}</p>`
+							)
+					)
+					.addTo(this.map)
 			})
 		}
 	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 	#map {
 		width: 100vw;
 		height: 94vh;
+
+		.marker {
+			background: hotpink;
+			width: 50px;
+			height: 50px;
+			border-radius: 50%;
+			cursor: pointer;
+		}
 	}
+
 </style>
